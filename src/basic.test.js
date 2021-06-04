07e481fae523510072a879daa4494854974eef7e
petrikovandrey <aapetrikov@gmail.com>
2021-06-04 09:46:02 +0300

    not to Be

--- src/basic.test.js
+++ src/basic.test.js
@@ -16,13 +16,17 @@ test("sort descending", () => {
     expect(result).toEqual(expected);
 })
 
-test("sort descending to Be", () => {
+test("sort descending not to Be", () => {
     let list = [
         { name: 'мечник', health: 10 },
         { name: 'маг', health: 100 },
         { name: 'лучник', health: 80 },
     ];
     const result = sortHealth(list);
-
-    expect(result[0].name).toBe("маг");
+    const expected = [
+        { name: 'мечник', health: 10 },
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 }
+    ];
+    expect(result).not.toBe(expected);
 })
